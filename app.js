d2915082bebfd08870613e6353e38bfcbe2af936
boris <boris@pumpkin.fun>
2026-01-18 13:40:26 +0500

    patient module removed

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectWithDB } from "./db.js";
 import todoRouter from "./routes/todoRoutes.js";
-import patientRouter from "./routes/patientRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 import authMiddleware from "./middlewares/authMiddleware.js";
 const app = express();
@@ -11,7 +10,6 @@ dotenv.config();
 connectWithDB();
 
 app.use("/todo-app",authMiddleware, todoRouter);
-app.use("/patient-app", patientRouter);
 app.use("/auth", authRouter);
 
 app.listen(5000, () => {
--- routes/patientRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addPatient, getPatient } from "../controllers/patientControllers.js";
-
-const router = express.Router();
-
-router.post("/patient", addPatient);
-router.get("/patient", getPatient);
-
-export default router;
